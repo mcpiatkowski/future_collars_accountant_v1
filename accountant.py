1cc493e8e6aec361b065b2fe480e93d631f9da86
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-01-11 21:06:52 +0100

    brak obsługi sys

--- accountant.py
+++ accountant.py
@@ -3,7 +3,7 @@
 #  import sys
 
 
-def buy(identification, price, amount):
+def trade(identification, price, amount):
     repeat = False
     #  sprawdzamy czy dodawany produkt znajduje się już w magazynie
     for i in warehouse:
@@ -11,36 +11,73 @@ def buy(identification, price, amount):
             repeat = True
             break
     # jeśli tak to dodajemy wskazaną ilość, jeśli nie dodajemy nowy produkt do magazynu
-    if repeat:
+    if repeat and command[0] == 'zakup':
         i[1] += command[3]
+    elif repeat and command[0] == 'sprzedaż':
+        i[1] -= command[3]
     else:
         warehouse.append([identification, amount])
     return price*amount
 
 
-#  def sell(identification, price, amount):
-
-balance = 0
-warehouse = []
 command = []
+brake_while = ['stop', 'konto', 'magazyn', 'przegląd']
+warehouse = []
+history = []
+balance = 0
 
 #  command = [str(sys.argv[1]), str(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4])]
 
+#  Pierwsza, modyfikująca część programu
+
 while command != 'stop':
-    command.append(input("Jaką czynność chcesz wykonać? "))
-    if command[0] == 'stop':
+
+    command.append(input())
+    print("Komenda: ", command)
+
+    if command[0] in brake_while:
         break
     if command[0] == 'saldo':
-        command.append(int(input("Jaka kwota? ")))
-        command.append(str(input("Komentarz: ")))
+        command.append(int(input()))
+        command.append(str(input()))
         balance += int(command[1])
-    if command[0] == 'zakup':
-        command.append(input("Jaki towar? "))
-        command.append(int(input("Jaka cena? ")))
-        command.append(int(input("Jaka ilość? ")))
-        balance -= buy(command[1], command[2], command[3])
-    command = []
-    print("Balance: {}$".format(balance))
-    print("Cały magazyn: ", warehouse)
+        print(command[1:])
+        print("Stan konta wynosi {}$.".format(balance))
+        if balance <0:
+            balance -= int(command[1])
+            print("Błąd! Ujemne saldo! Maksymalnie do wypłaty: ", balance)
+    elif command[0] == 'zakup' or command[0] == 'sprzedaż':
+        command.append(input())
+        command.append(int(input()))
+        command.append(int(input()))
+        if command[2] < 0 or command[3] < 0:
+            print("Wartość musi być większa bądź równa zero!")
+            continue
+        if command[0] == 'zakup':
+            balance -= trade(command[1], command[2], command[3])
+        else:
+            balance += trade(command[1], command[2], command[3])
+        print(command[1:])
+        print("Stan konta wynosi {}$.".format(balance))
+    else:
+        print("Błąd! Nieznana komenda! Podsumowanie:")
+        break
 
-#  Komendy z klawiatury
+    history += [command]
+    command = []
+
+
+if command[0] == 'konto':
+    print("Stan konta wynosi {}$.".format(balance))
+if command[0] == 'magazyn':
+    print("Cały magazyn: ", warehouse)
+if command[0] == 'przegląd':
+    print("Historia zleceń: ")
+    for i in history:
+        print(i)
+
+print("Stan konta wynosi: {}$.".format(balance))
+print("Cały magazyn: ", warehouse)
+print("Historia zleceń: ")
+for i in history:
+    print(i)
