47606c023c709af72cbc3ef666437ab57a4a5e5b
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-01-12 11:36:29 +0100

    bez obsługi żadnych błędów

--- accountant.py
+++ accountant.py
@@ -1,83 +1,71 @@
 #  ACCOUNTANT
 
 #  import sys
-
-
-def trade(identification, price, amount):
-    repeat = False
-    #  sprawdzamy czy dodawany produkt znajduje się już w magazynie
-    for i in warehouse:
-        if i[0] == command[1]:
-            repeat = True
-            break
-    # jeśli tak to dodajemy wskazaną ilość, jeśli nie dodajemy nowy produkt do magazynu
-    if repeat and command[0] == 'zakup':
-        i[1] += command[3]
-    elif repeat and command[0] == 'sprzedaż':
-        i[1] -= command[3]
-    else:
-        warehouse.append([identification, amount])
-    return price*amount
-
-
 command = []
-brake_while = ['stop', 'konto', 'magazyn', 'przegląd']
-warehouse = []
+warehouse = {}
 history = []
 balance = 0
 
-#  command = [str(sys.argv[1]), str(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4])]
 
-#  Pierwsza, modyfikująca część programu
-
-while command != 'stop':
-
-    command.append(input())
-    print("Komenda: ", command)
-
-    if command[0] in brake_while:
-        break
-    if command[0] == 'saldo':
-        command.append(int(input()))
-        command.append(str(input()))
-        balance += int(command[1])
-        print(command[1:])
-        print("Stan konta wynosi {}$.".format(balance))
-        if balance <0:
-            balance -= int(command[1])
-            print("Błąd! Ujemne saldo! Maksymalnie do wypłaty: ", balance)
-    elif command[0] == 'zakup' or command[0] == 'sprzedaż':
-        command.append(input())
-        command.append(int(input()))
-        command.append(int(input()))
-        if command[2] < 0 or command[3] < 0:
-            print("Wartość musi być większa bądź równa zero!")
-            continue
-        if command[0] == 'zakup':
-            balance -= trade(command[1], command[2], command[3])
-        else:
-            balance += trade(command[1], command[2], command[3])
-        print(command[1:])
-        print("Stan konta wynosi {}$.".format(balance))
+def trade(price, amount):
+    if (command[1] in warehouse) and command[0] == 'zakup':
+        warehouse[command[1]] += command[3]
+    elif command[1] in warehouse and command[0] == 'sprzedaż':
+        warehouse[command[1]] -= command[3]
     else:
-        print("Błąd! Nieznana komenda! Podsumowanie:")
+        warehouse.update({command[1]: command[3]})
+    return price*amount
+
+
+def f_command(action):
+    action = [action]
+    if action[0] == 'zakup' or action[0] == 'sprzedaż':
+        action.append(input())
+        action.append(int(input()))
+        action.append(int(input()))
+    elif action[0] == 'saldo':
+        action.append(int(input()))
+        action.append(str(input()))
+    else:
+        action[0] = 'stop'
+        print("STOP")
+    return action
+
+
+def check_values(values, cash):
+    if values[0] == 'saldo' and cash < 0:
+        cash -= int(values[1])
+        print("Ujemne saldo! Maksymalnie do wypłaty: ", cash)
+        return cash
+    elif (values[0] == 'zakup' or values[0] == 'sprzedaż') and (values[2] < 0 or values[3] < 0):
+        print("Błąd! Cena oraz ilość muszą być dodatnie!")
+        return False
+    else:
+        return cash
+
+
+while True:
+
+    command = f_command(input())
+    print("Aktualna komenda: ", command)
+
+    if command[0] == 'stop':
         break
+    elif command[0] == 'saldo':
+        balance += int(command[1])
+        balance = check_values(command, balance)
+    elif command[0] == 'zakup':
+        balance -= trade(command[2], command[3])
+        print("Balance: ", balance)
+        print("Magazyn: ", warehouse)
+    elif command[0] == 'sprzedaż':
+        balance += trade(command[2], command[3])
+        print("Balance: ", balance)
+        print("Magazyn: ", warehouse)
 
     history += [command]
-    command = []
 
+print("Balance: ", balance)
+for record in history:
+    print("Historia: ", record)
 
-if command[0] == 'konto':
-    print("Stan konta wynosi {}$.".format(balance))
-if command[0] == 'magazyn':
-    print("Cały magazyn: ", warehouse)
-if command[0] == 'przegląd':
-    print("Historia zleceń: ")
-    for i in history:
-        print(i)
-
-print("Stan konta wynosi: {}$.".format(balance))
-print("Cały magazyn: ", warehouse)
-print("Historia zleceń: ")
-for i in history:
-    print(i)
