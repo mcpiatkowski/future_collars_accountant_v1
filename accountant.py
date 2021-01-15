82bdd5c94430904c8f3fbe3a4a015396f4e5fb9c
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-01-15 18:35:41 +0100

    Brak wypisywania historii, może generować błędy

--- accountant.py
+++ accountant.py
@@ -2,6 +2,13 @@
 
 import sys
 
+command = []
+warehouse = {}
+history = []
+brake_while = ['stop', 'konto', 'magazyn', 'przegląd']
+allow = ['konto', 'magazyn', 'przegląd', 'saldo', 'zakup', 'sprzedaż']
+balance = 0
+
 
 def trade(price, amount):
     if (command[1] in warehouse) and command[0] == 'zakup':
@@ -11,6 +18,20 @@ def trade(price, amount):
     else:
         warehouse.update({command[1]: command[3]})
     return price*amount
+
+
+def product_check():
+    for item in command[1:]:
+        try:
+            print('{}: {}'.format(item, warehouse[item]))
+        except KeyError:
+            print('{}: 0'.format(item))
+
+
+def summary():
+    for index in range(int(command[1]), int(command[2])+1):
+        for action in history[index]:
+            print(action)
 
 
 def grab_command(action):
@@ -26,7 +47,18 @@ def grab_command(action):
 
 def grab_argv():
     command = []
-    if len(sys.argv) == 4:
+    if len(sys.argv) == 1:
+        return command
+    if sys.argv[1] == 'magazyn':
+        command.append(str(sys.argv[1]))
+        for item in range(2, len(sys.argv)):
+            command.append(str(sys.argv[item]))
+        return command
+    if sys.argv[1] == 'przegląd':
+        command.append(str(sys.argv[1]))
+        command.append(int(sys.argv[2]))
+        command.append(int(sys.argv[3]))
+    if sys.argv[1] == 'saldo':
         command.append(str(sys.argv[1]))
         command.append(int(sys.argv[2]))
         command.append(str(sys.argv[3]))
@@ -34,7 +66,7 @@ def grab_argv():
             return print("Błąd! Nieznana komenda.")
         if balance + command[1] < 0:
             return print("Błąd! Brak środków.")
-    elif len(sys.argv) == 5:
+    elif sys.argv[1] == 'zakup' or sys.argv[1] == 'sprzedaż':
         command.append(str(sys.argv[1]))
         command.append(str(sys.argv[2]))
         command.append(int(sys.argv[3]))
@@ -43,8 +75,9 @@ def grab_argv():
             return print("Błąd! Nieznana komenda.")
         if command[2] < 0 or command[3] < 0:
             return print("Błąd! Ilość musi być dodatnia.")
-    else:
-        command.append(str(sys.argv[1]))
+    elif len(sys.argv) == 2:
+        if sys.argv[1] == 'konto':
+            command.append(str(sys.argv[1]))
     return command
 
 
@@ -65,23 +98,16 @@ def error(values, cash, stock):
     return True
 
 
-command = []
-warehouse = {}
-history = []
-brake_while = ['stop', 'konto', 'magazyn', 'przegląd']
-allow = ['konto', 'magazyn', 'przegląd', 'saldo', 'zakup', 'sprzedaż']
-balance = 0
-
-
 while True:
-
     command = grab_command(input())
     check = error(command, balance, warehouse)
-    history += command
+    history += [command]
     if not check:
         break
-    if command[0] in brake_while:
+    if command[0] == 'stop':
         command = grab_argv()
+        if len(sys.argv) == 1:
+            break
     if command[0] == 'saldo':
         balance += int(command[1])
     if command[0] == 'zakup':
@@ -90,24 +116,16 @@ while True:
         balance += trade(command[2], command[3])
     if command[0] == 'konto':
         print("Balance: ", balance)
-    if history[-1] == 'stop':
+    if command[0] == 'magazyn':
+        product_check()
+    if command[0] == 'przegląd':
+        summary()
+    if history[-1] == ['stop']:
         stop = history.pop(-1)
-        history += command
+        history += [command]
         history.append(stop)
         break
 
-for record in history:
-        print(record)
-
-print("Balance: ", balance)
-#elif command[0] == 'magazyn':
-#    for item, amount in warehouse.items():
-#        print(item, amount)
-#elif command[0] == 'przegląd':
-#    print("Historia:")
-#    for record in history:
-#        print(record)
-#elif check:
-#    for record in history:
-#        for i in record:
-#            print(i)
+#for index in history:
+#    for action in index:
+#        print(action)
