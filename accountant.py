b9ac2187d2797f972ee7f94a4602504b7e801647
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-01-23 12:47:35 +0100

    Obsługa zewnętrznego pliku wejściowego.

--- accountant.py
+++ accountant.py
@@ -1,83 +1,186 @@
-#  ACCOUNTANT
+import sys
 
-#  import sys
+command = []
+warehouse = {}
+history = []
+balance = 0
+print_history = ['saldo', 'zakup', 'sprzedaz']
+argv_break = ['przegląd', 'saldo', 'zakup', 'sprzedaz', 'konto', 'magazyn']
+allow = 0
+input_from_file = []
 
 
-def trade(identification, price, amount):
-    repeat = False
-    #  sprawdzamy czy dodawany produkt znajduje się już w magazynie
-    for i in warehouse:
-        if i[0] == command[1]:
-            repeat = True
-            break
-    # jeśli tak to dodajemy wskazaną ilość, jeśli nie dodajemy nowy produkt do magazynu
-    if repeat and command[0] == 'zakup':
-        i[1] += command[3]
-    elif repeat and command[0] == 'sprzedaż':
-        i[1] -= command[3]
+def add_to_command():
+    if input_from_file[0] == 'zakup' or input_from_file[0] == 'sprzedaz':
+        command.append(input_from_file[0])
+        command.append(input_from_file[1])
+        command.append(int(input_from_file[2]))
+        command.append(int(input_from_file[3]))    
+    elif input_from_file[0] == 'saldo':
+        command.append(input_from_file[0])
+        command.append(int(input_from_file[1])) 
+        command.append(input_from_file[2])
+    elif input_from_file[0] == 'stop':
+        command.append(input_from_file[0])
+
+
+def delete_from_input():
+    if input_from_file[0] == 'zakup' or input_from_file[0] == 'sprzedaz':
+        for count in range(4):
+            del input_from_file[0]
+    elif input_from_file[0] == 'saldo':
+        for count in range(3):
+            del input_from_file[0]
+    elif input_from_file[0] == 'stop':
+        del input_from_file[0]
+
+
+def grab_input():
+    with open('in.txt') as input_txt:
+        for line in input_txt:
+            line = line.strip()
+            input_from_file.append(line)
+
+
+def grab_command():
+    if input_from_file[0] == 'saldo':
+        add_to_command()
+        delete_from_input()
+    elif input_from_file[0] == 'zakup' or input_from_file[0] == 'sprzedaz':
+        add_to_command()
+        delete_from_input()
+    elif input_from_file[0] == 'stop':
+        add_to_command()
+        delete_from_input()
+
+
+def trade(price, amount):
+    if (command[1] in warehouse) and command[0] == 'zakup':
+        warehouse[command[1]] += int(command[3])
+    elif command[1] in warehouse and command[0] == 'sprzedaz':
+        warehouse[command[1]] -= int(command[3])
     else:
-        warehouse.append([identification, amount])
+        warehouse.update({command[1]: int(command[3])})
     return price*amount
 
 
-command = []
-brake_while = ['stop', 'konto', 'magazyn', 'przegląd']
-warehouse = []
-history = []
-balance = 0
+def product_check():
+    for item in command[1:]:
+        try:
+            print('{}: {}'.format(item, warehouse[item]))
+        except KeyError:
+            print('{}: 0'.format(item))
 
-#  command = [str(sys.argv[1]), str(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4])]
 
-#  Pierwsza, modyfikująca część programu
+def summary():
+    for index in range(int(command[1]), int(command[2])+1):
+        for action in history[index]:
+            print("podsumowanie: ", action)
 
-while command != 'stop':
 
-    command.append(input())
-    print("Komenda: ", command)
-
-    if command[0] in brake_while:
-        break
-    if command[0] == 'saldo':
-        command.append(int(input()))
-        command.append(str(input()))
-        balance += int(command[1])
-        print(command[1:])
-        print("Stan konta wynosi {}$.".format(balance))
-        if balance <0:
-            balance -= int(command[1])
-            print("Błąd! Ujemne saldo! Maksymalnie do wypłaty: ", balance)
-    elif command[0] == 'zakup' or command[0] == 'sprzedaż':
-        command.append(input())
-        command.append(int(input()))
-        command.append(int(input()))
-        if command[2] < 0 or command[3] < 0:
-            print("Wartość musi być większa bądź równa zero!")
-            continue
-        if command[0] == 'zakup':
-            balance -= trade(command[1], command[2], command[3])
-        else:
-            balance += trade(command[1], command[2], command[3])
-        print(command[1:])
-        print("Stan konta wynosi {}$.".format(balance))
-    else:
-        print("Błąd! Nieznana komenda! Podsumowanie:")
-        break
-
-    history += [command]
+def grab_argv():
     command = []
+    if len(sys.argv) == 1:
+        return command
+    if sys.argv[1] == 'magazyn':
+        command.append(str(sys.argv[1]))
+        for item in range(2, len(sys.argv)):
+            command.append(str(sys.argv[item]))
+        return command
+    if sys.argv[1] == 'przegląd':
+            command.append(str(sys.argv[1]))
+            command.append(int(sys.argv[2]))
+            command.append(int(sys.argv[3]))
+    if sys.argv[1] == 'saldo':
+        command.append(str(sys.argv[1]))
+        command.append(int(sys.argv[2]))
+        command.append(str(sys.argv[3]))
+        if balance + command[1] < 0:
+            return print("Błąd! Brak środków.")
+    elif sys.argv[1] == 'zakup' or sys.argv[1] == 'sprzedaz':
+        command.append(str(sys.argv[1]))
+        command.append(str(sys.argv[2]))
+        command.append(int(sys.argv[3]))
+        command.append(int(sys.argv[4]))
+        if command[2] < 0 or command[3] < 0:
+            return print("Błąd! Ilość musi być dodatnia.")
+    elif len(sys.argv) == 2:
+        if sys.argv[1] == 'konto':
+            command.append(str(sys.argv[1]))
+    return command
 
 
-if command[0] == 'konto':
-    print("Stan konta wynosi {}$.".format(balance))
-if command[0] == 'magazyn':
-    print("Cały magazyn: ", warehouse)
-if command[0] == 'przegląd':
-    print("Historia zleceń: ")
-    for i in history:
-        print(i)
+def error(values, cash, stock):
+    for i in values:
+        if values[0] == 'saldo' and cash + values[1] < 0:
+            print("Błąd! Ujemne saldo!")
+            return False
+        if values[0] != 'saldo' and type(i) == int and i < 0:
+            print("Błąd! Ujemne wartości!")
+            return False
+    if values[0] == 'zakup' and cash - values[2] * values[3] < 0:
+        print("Błąd! Nie masz wystarczającej ilości środków")
+        return False
+    if values[0] == 'sprzedaz' and values[3] > stock[values[1]]:
+        print("Błąd! Brak wystarczającej ilości sztuk na magazynie.")
+        return False
+    return True
 
-print("Stan konta wynosi: {}$.".format(balance))
-print("Cały magazyn: ", warehouse)
-print("Historia zleceń: ")
-for i in history:
-    print(i)
+
+def argv_error():
+    if sys.argv[1] == 'przegląd' and len(sys.argv) < 4:
+        print("Błąd! Za mało argumentów.")
+        return False
+    if sys.argv[1] == 'saldo' and len(sys.argv) < 4:
+        print("Błąd! Za mało argumentów.")
+        return False
+    elif (sys.argv[1] == 'zakup' or sys.argv[1] == 'sprzedaz') and len(sys.argv) < 5:
+        print("Błąd! Za mało argumentów.")
+        return False
+    elif sys.argv[1] not in argv_break:
+        print("Błąd! Za mało argumentów.")
+        return False
+    return True
+
+
+grab_input()
+
+while True:
+    command = []
+    grab_command()
+    check = error(command, balance, warehouse)
+    history += [command]
+    if not check:
+        break
+    if command[0] == 'stop':
+        argv_check = argv_error()
+        if argv_check == False:
+            break
+        command = grab_argv()
+        if len(sys.argv) == 1:
+            allow = 1
+            break
+        if command[0] in print_history:
+            allow =1
+    if command[0] == 'saldo':
+        balance += int(command[1])
+    if command[0] == 'zakup':
+        balance -= trade(int(command[2]), int(command[3]))
+    if command[0] == 'sprzedaz':
+        balance += trade(command[2], command[3])
+    if command[0] == 'konto':
+        print("Balance: ", balance)
+    if command[0] == 'magazyn':
+        product_check()
+    if command[0] == 'przegląd':
+        summary()
+    if history[-1] == ['stop']:
+        stop = history.pop(-1)
+        history += [command]
+        history.append(stop)
+        break
+
+if allow == 1:
+    for index in history:
+        for action in index:
+            print(action)
